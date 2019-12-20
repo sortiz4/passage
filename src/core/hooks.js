b72b44c627b94262ec10570cdd0126f5858efb14
Steven Ortiz <sortiz832@gmail.com>
2019-12-20 09:41:38 -0600

    Updated hooks

--- src/core/hooks.js
+++ src/core/hooks.js
@@ -10,19 +10,33 @@ export function useClassState(constructor, initial) {
             [constructor, initial],
         )
     );
-    const [_, setState] = React.useReducer(
-        (prevState, updater) => {
-            if(typeof updater !== 'function') {
-                Object.assign(state.current, updater);
-            } else {
-                updater(state.current);
-            }
-            return !prevState;
-        },
-        true,
-    );
-    return [state.current, setState];
+    const params = useParams(React.useState(true));
+    return [
+        state.current,
+        React.useCallback(
+            updater => {
+                console.log('set state called');
+                const [signal, setSignal] = params.current;
+                if(typeof updater !== 'function') {
+                    Object.assign(state.current, updater);
+                } else {
+                    updater(state.current);
+                }
+                setSignal(!signal);
+            },
+            [state, params],
+        ),
+    ];
+}
+
+/**
+ * Reduces invalidation by storing parameters in a stable container.
+ */
+export function useParams(current) {
+    const params = React.useRef(current);
+    params.current = current;
+    return params;
 }
 
 // Module namespace
-export const Hooks = {useClassState};
+export const Hooks = {useClassState, useParams};
